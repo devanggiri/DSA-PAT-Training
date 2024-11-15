baee938d5b8722eb0daac9720686cd1385e637d1
Devang Giri Goswami <167710924+devanggiri@users.noreply.github.com>
2024-11-15 14:48:55 +0530

    Create Bucket Sort.py

---
+++ Sorting Methods/Bucket Sort.py
@@ -0,0 +1,13 @@
+def bucket_sort(arr):
+    bucket = [[] for _ in range(len(arr))]
+    for num in arr:
+        index = int(num * len(arr))
+        bucket[index].append(num)
+
+    for i in range(len(arr)):
+        bucket[i] = sorted(bucket[i])
+
+    result = []
+    for sublist in bucket:
+        result.extend(sublist)
+    return result
