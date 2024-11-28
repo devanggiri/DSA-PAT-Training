08dd3d7f4c4e1764a325ba3fef8f732a3c00f3b1
Devang Giri Goswami <167710924+devanggiri@users.noreply.github.com>
2024-11-28 21:30:03 +0530

    Create Disjoint Set.py

---
+++ Data Structures/Disjoint Set.py
@@ -0,0 +1,19 @@
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = list(range(n))
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            self.parent[root_y] = root_x
+
+# Example usage
+ds = DisjointSet(5)
+ds.union(0, 1)
+print(ds.find(1))  # Output: 0
