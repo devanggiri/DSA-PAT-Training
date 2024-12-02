95569990da201ec54482a727872c993311777b25
Devang Giri Goswami <167710924+devanggiri@users.noreply.github.com>
2024-12-02 18:13:44 +0530

    Create Union-Find Disjoint Set Union.py

---
+++ Algos/Union-Find Disjoint Set Union.py
@@ -0,0 +1,28 @@
+class UnionFind:
+    def __init__(self, size):
+        self.root = list(range(size))
+        self.rank = [1] * size
+
+    def find(self, x):
+        if self.root[x] != x:
+            self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+
+# Test
+uf = UnionFind(5)
+uf.union(0, 1)
+uf.union(1, 2)
+print(uf.find(0) == uf.find(2))  # Output: True
+print(uf.find(0) == uf.find(3))  # Output: False
